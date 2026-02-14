39437e13e3f0a675ab3299849e9e2c44e8646d2b
Morrison <nj.mrrsn@gmail.com>
2026-02-13 20:23:33 -0500

    add terraform_fmt to conform

--- .config/nvim/lua/mojovim/lazy/conform.lua
+++ .config/nvim/lua/mojovim/lazy/conform.lua
@@ -1,23 +1,24 @@
 return {
-  'stevearc/conform.nvim',
-  opts = {},
-  config = function()
-    local conform = require("conform")
-    conform.setup({
-      formatters_by_ft = {
-        lua = { "stylua" },
-        go = { "goimports", "gofmt" },
-        typescript = { "prettier" },
-        typescriptreact = { "prettier" },
-        javascript = { "prettier" },
-        javascriptreact = { "prettier" },
-      }
-    })
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      pattern = "*",
-      callback = function(args)
-        conform.format({ bufnr = args.buf })
-      end,
-    })
-  end
+	"stevearc/conform.nvim",
+	opts = {},
+	config = function()
+		local conform = require("conform")
+		conform.setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				go = { "goimports", "gofmt" },
+				typescript = { "prettier" },
+				typescriptreact = { "prettier" },
+				javascript = { "prettier" },
+				javascriptreact = { "prettier" },
+				terraform = { "terraform_fmt" },
+			},
+		})
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*",
+			callback = function(args)
+				conform.format({ bufnr = args.buf })
+			end,
+		})
+	end,
 }
